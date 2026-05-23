04d199d7abc329ff160d6da1567ddefd816f437a
Anshul253 <143690339+Anshul253@users.noreply.github.com>
2026-05-23 15:42:58 +0530

    Allow IIT Bombay image hostnames

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,9 @@ const nextConfig: NextConfig = {
     remotePatterns: [
       { protocol: "https", hostname: "images.unsplash.com" },
       { protocol: "https", hostname: "upload.wikimedia.org" },
+      { protocol: "https", hostname: "iitb-wustl.org" },
+      { protocol: "https", hostname: "c.ndtvimg.com" },
+      { protocol: "https", hostname: "mcdonnell.washu.edu" },
     ],
   },
 };
